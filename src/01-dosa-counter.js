d649f32807f871873260da81f5ecb9ffd52d0757
Swagatika Sahoo <swagatika07@gmail.com>
2026-02-27 00:08:53 -0500

    complete- dosa-counter

--- src/01-dosa-counter.js
+++ src/01-dosa-counter.js
@@ -33,4 +33,15 @@
  */
 export function calculateDosaOrder(type, quantity = 1, isSpicy = false) {
   // Your code here
+const dosa = { plain: 40, masala: 60, onion: 50, butter: 70, paper: 90, cheese: 80}
+
+if( typeof type != "string" || !Object.hasOwn(dosa , type)) return null;
+if( !Number.isFinite(quantity) || typeof quantity != "number" || quantity <=0) return null;
+if (typeof isSpicy !== "boolean") return null;
+
+   const basePrice = dosa[type];
+  const pricePerDosa = basePrice + (isSpicy ? 10 : 0);
+  const total = pricePerDosa * quantity;
+   
+  return { type, quantity, pricePerDosa, total };
 }
